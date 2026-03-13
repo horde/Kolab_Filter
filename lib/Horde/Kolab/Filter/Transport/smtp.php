1b748674522c054a243bc10bb5c09e20f1a90019
Ralf Lang <ralf.lang@ralf-lang.de>
2026-03-13 14:05:23 +0100

    feat: Use horde/smtp instead of pear/net_smtp

--- lib/Horde/Kolab/Filter/Transport/smtp.php
+++ lib/Horde/Kolab/Filter/Transport/smtp.php
@@ -1,12 +1,9 @@
 <?php
 /**
- * @package Kolab_Filter
- */
-
-/**
  * Provides SMTP for delivering mail.
  *
  * Copyright 2004-2008 Klarälvdalens Datakonsult AB
+ * Copyright 2026 Horde LLC (http://www.horde.org/)
  *
  * See the enclosed file LICENSE for license information (LGPL). If you
  * did not receive this file, see http://www.horde.org/licenses/lgpl21.
@@ -20,12 +17,10 @@ class Horde_Kolab_Filter_Transport_smtp extends Horde_Kolab_Filter_Transport
     /**
      * Create the transport handler.
      *
-     * @return Net_SMTP The SMTP handler.
+     * @return Horde_Smtp The SMTP handler.
      */
     function _createTransport()
     {
-        require_once 'Net/SMTP.php';
-
         if (!isset($this->_params['host'])) {
             $this->_params['host'] = '127.0.0.1';
         }
@@ -34,8 +29,14 @@ class Horde_Kolab_Filter_Transport_smtp extends Horde_Kolab_Filter_Transport
             $this->_params['port'] = 25;
         }
 
-        $transport = new Net_SMTP($this->_params['host'],
-                                  $this->_params['port']);
+        // Use Horde_Smtp instead of pear/net_smtp
+        $transport = new Horde_Smtp([
+            'host' => $this->_params['host'],
+            'port' => $this->_params['port'],
+            'secure' => false,  // Kolab uses local unencrypted SMTP
+            'timeout' => $this->_params['timeout'] ?? 10
+        ]);
+
         return $transport;
     }
 }
